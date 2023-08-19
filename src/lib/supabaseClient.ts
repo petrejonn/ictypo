80f9b0b148b60e741d228f1c53ec8440f2e40768
petrejonn <petrejonn@gmail.com>
2023-08-19 11:00:03 +0100

    Use env to save supabase key

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://luvoaucvcfiwiegiseej.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1dm9hdWN2Y2Zpd2llZ2lzZWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA2MjA3MjEsImV4cCI6MjAwNjE5NjcyMX0.C6WVL5QUacPZEbYKbAHeMa5QAJvPy8695ZRs4oqc-NI'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY
+);
